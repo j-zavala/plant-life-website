13e3e90d45bf697ac8f0890b98ceabfd6c4c7e97
Johnny Zavala <jzavalafgz@gmail.com>
2018-09-03 17:03:26 -0400

    Fixed slide error: slides stopped after 4 clicks, added looping functionality

--- assets/slides.js
+++ assets/slides.js
@@ -9,6 +9,11 @@ let z = 1
 // when i click the slidearea, change the slide based on the z-index
 slideArea.addEventListener("click", function () {
     currentSlide = currentSlide + 1
+
+    if (currentSlide > images.length - 1) {
+        currentSlide = 0
+    }
+
     z = z + 1
 
     //pick the right image
